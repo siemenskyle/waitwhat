c7c64c1b35c339dd658309e23c306a13118b1531
Shane <williamson.shane.a@gmail.com>
2016-02-19 22:06:27 -0600

    New Actor class and Object interface

---
+++ Assets/Actor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actor : MonoBehaviour {
+
+    // Properties/Fields
+    private bool alive;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Getter for 'alive' field
+    public bool isAlive()
+    {
+        return this.alive;
+    }
+
+    // Setter for the 'alive' field
+    public void die()
+    {
+        this.alive = false;
+    }
+}
---
+++ Assets/Object.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface Object {
+
+    // Interact function that any object would call on a trigger's enter
+    void interact();
+
+}
