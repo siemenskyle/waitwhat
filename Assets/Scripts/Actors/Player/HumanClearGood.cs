e72ba13b91efea9884aa1446553247e335690919
siemenskyle <siemenskyle@gmail.com>
2016-02-20 22:12:05 -0600

    Revert "fix switches"
    This reverts commit 7a85b32dbd5c8ae5a362ef444479bb4154787dcc.

--- Assets/Scripts/Actors/Player/HumanClearGood.cs
+++ Assets/Scripts/Actors/Player/HumanClearGood.cs
@@ -6,10 +6,7 @@ public class HumanClearGood : MonoBehaviour {
 	public GameObject checks;
 
 	void OnTriggerStay2D(Collider2D other){
-		if( other.isTrigger )
-			checks.GetComponent<ParTransferGood> ().amClear = true;
-		else
-			checks.GetComponent<ParTransferGood> ().amClear = false;
+		checks.GetComponent<ParTransferGood> ().amClear = false;
 	}
 
 	void OnTriggerExit2D(Collider2D other){
