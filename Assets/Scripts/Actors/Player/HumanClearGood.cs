156ee5f0f5256fab63ae517e838459cc3442bb67
siemenskyle <siemenskyle@gmail.com>
2016-02-21 11:30:39 -0600

    change switches and goals to be on trigger layer, circle check on check layer
    trigger layer and check layer cannot see each other, fixes some
    switching issues with switching onto switches

--- Assets/Scripts/Actors/Player/HumanClearGood.cs
+++ Assets/Scripts/Actors/Player/HumanClearGood.cs
@@ -6,13 +6,17 @@ public class HumanClearGood : MonoBehaviour {
 	public GameObject checks;
 
 	void OnTriggerStay2D(Collider2D other){
-		if( other.isTrigger )
-			checks.GetComponent<ParTransferGood> ().amClear = true;
-		else
+		//if( other.isTrigger )
+		//	checks.GetComponent<ParTransferGood> ().amClear = true;
+		//else
 			checks.GetComponent<ParTransferGood> ().amClear = false;
 	}
 
 	void OnTriggerExit2D(Collider2D other){
 		checks.GetComponent<ParTransferGood> ().amClear = true;
+	}
+
+	void Update(){
+
 	}
 }
