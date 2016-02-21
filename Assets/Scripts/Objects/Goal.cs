08eb51bba10be327bc2bca874743703e53fcd392
siemenskyle <siemenskyle@gmail.com>
2016-02-20 18:19:54 -0600

    goal manager, level manager made, levels have goals added

--- Assets/Scripts/Objects/Goal.cs
+++ Assets/Scripts/Objects/Goal.cs
@@ -1,19 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour {
 
     // Fields
-    private int playersInGoal;
+	public GoalManager manager;
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         // Check to see if the triggering object is a human
         if (other.gameObject.tag == "Player")
         {
-            // If it is an Actor, then interact with it
-            advanceToNextLevel();
+            // If it is a Player, then interact with it
+			manager.inGoal += 1;
         }
     }
 
@@ -23,13 +22,13 @@ public class Goal : MonoBehaviour {
         if (other.gameObject.tag == "Player")
         {
             // Decrement the number of players in the goal region.
-            playersInGoal -= 1;
+            manager.inGoal -= 1;
         }
     }
 
     // Use this for initialization
     void Start () {
-        playersInGoal = 0;
+		
 	}
 	
 	// Update is called once per frame
@@ -37,11 +36,5 @@ public class Goal : MonoBehaviour {
 	
 	}
 
-    // Advance to the next level
-    public void advanceToNextLevel()
-    {
-        // Since the goal has been interacted with, we then move to the next level.
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
 
 }
---
+++ Assets/Scripts/Objects/GoalManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoalManager : MonoBehaviour {
+
+	public int inGoal;
+
+	// Use this for initialization
+	void Start () {
+		inGoal = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (inGoal == 2)
+			advanceToNextLevel ();
+	}
+
+
+	// Advance to the next level
+	public void advanceToNextLevel()
+	{
+		// Since the goal has been interacted with, we then move to the next level.
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
