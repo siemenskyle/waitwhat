a1da51237ddb7dddb10d26f21c11823cadc39d89
ConnorNG <netco_nessa@hotmail.com>
2016-02-20 23:04:08 -0600

    made people fade out when switched to/from
    changes in ParTransferGood

--- Assets/Scripts/Actors/Player/Movement.cs
+++ Assets/Scripts/Actors/Player/Movement.cs
@@ -18,6 +18,27 @@ public class Movement : MonoBehaviour {
 
 		float y = Input.GetAxis (keys.getYAxis());
 
+		if (x > 0) {
+			this.GetComponent<Animator> ().SetBool ("right", true);
+			this.GetComponent<Animator> ().SetBool ("left", false);
+		} else if (x < 0) {
+			this.GetComponent<Animator> ().SetBool ("left", true);
+			this.GetComponent<Animator> ().SetBool ("right", false);
+		} else {
+			this.GetComponent<Animator> ().SetBool ("left", false);
+			this.GetComponent<Animator> ().SetBool ("right", false);
+		}
+
+		if (y > 0) {
+			this.GetComponent<Animator> ().SetBool ("up", true);
+			this.GetComponent<Animator> ().SetBool ("down", false);
+		} else if (y < 0) {
+			this.GetComponent<Animator> ().SetBool ("up", true);
+			this.GetComponent<Animator> ().SetBool ("down", false);
+		} else {
+			this.GetComponent<Animator> ().SetBool ("up", false);
+			this.GetComponent<Animator> ().SetBool ("down", false);
+		}
 
 		if ((x != 0 || y != 0) && this.transform.parent == null && this.GetComponent<Collider2D>().isTrigger == false) {
 			transform.Translate(Vector3.right * movespeed * Time.fixedDeltaTime * x + Vector3.up * movespeed * Time.fixedDeltaTime * y);
